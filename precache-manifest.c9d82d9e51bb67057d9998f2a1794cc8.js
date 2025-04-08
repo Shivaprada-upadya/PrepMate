ee832430e93b9a8ef662a833b7c47c4ffb193ca8
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-04-08 15:00:01 +0530

    Updates

---
+++ precache-manifest.c9d82d9e51bb67057d9998f2a1794cc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54c73efe621df68348ed10d808113f9",
+    "url": "/PrepMate/index.html"
+  },
+  {
+    "revision": "ef388bce6d51d6e2fc46",
+    "url": "/PrepMate/static/css/2.b24350a8.chunk.css"
+  },
+  {
+    "revision": "3811efd989ec96bdf6bd",
+    "url": "/PrepMate/static/css/main.07623e4d.chunk.css"
+  },
+  {
+    "revision": "ef388bce6d51d6e2fc46",
+    "url": "/PrepMate/static/js/2.8cd26d70.chunk.js"
+  },
+  {
+    "revision": "4cb447811eafcde62d61d67183d8b566",
+    "url": "/PrepMate/static/js/2.8cd26d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7261ae7bece3747e32ef",
+    "url": "/PrepMate/static/js/3.41463fe0.chunk.js"
+  },
+  {
+    "revision": "3811efd989ec96bdf6bd",
+    "url": "/PrepMate/static/js/main.af2024eb.chunk.js"
+  },
+  {
+    "revision": "fa2bc963ed62763f6e0e",
+    "url": "/PrepMate/static/js/runtime-main.ad89631b.js"
+  }
+]);
